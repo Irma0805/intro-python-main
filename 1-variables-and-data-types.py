b67bd79f8b69276c606da44af08889731ab58ee6
Irma0805 <irmajortizs@hotmail.es>
2026-06-03 18:58:28 +0200

    Complete variables and strings exercises

--- 1-variables-and-data-types.py
+++ 1-variables-and-data-types.py
@@ -9,7 +9,8 @@ Crea una variable llamada "mensaje".
 Asígnale el valor "¡Hola, Mundo!". 
 Imprime el valor de la variable en la consola.
 """
-# Escribe tu código aquí
+mensaje = "Hola, Mundo!"
+print(mensaje)
 
 """
 --- Ejercicio 2 Variables---
@@ -18,7 +19,9 @@ Reasígnale el valor "Hello world!".
 Imprime el valor de la variable en la consola.
 Escribe en un comentario de línea lo que sucede.
 """
-# Escribe tu código aquí
+mensaje = "Hello world!"
+print(mensaje)
+#Hemos cambiado la frase Hola mundo al inglés
 
 """
 --- Ejercicio 3 Tipos de datos---
@@ -26,5 +29,38 @@ Crea variables para cada uno de los siguientes tipos de datos y colecciones: str
 bool, list, tuple, dicctionary and set. 
 Imprime cada variable y el tipo de dato o colección que almacena en la consola.
 """
-# Escribe tu código aquí
+texto = "Banana"
+entero = 47
+flotante = 19.9
+boleano= True
+lista = ["perro", "gato", "conejo"]
+tupla = ("amarillo", "azul", "rojo")
+diccionario ={
+    "Nombre": "Irma",
+     "Edad": 47,
+    "Documento": 2346765
+}
+animales_domesticos = {"perro", "gato", "conejo", "perro"}
+
+print(texto)
+print(type(texto))
+print(entero)
+print(type(entero))
+print(flotante)
+print(type(flotante))
+print(boleano)
+print(type(boleano))
+print(lista)
+print(type(lista))
+print(tupla)
+print(type(tupla))
+print(diccionario)
+print(type(diccionario))
+print(animales_domesticos)
+print(type(animales_domesticos))
+
+
+
+
+
 
--- 2-strings-manipulation.py
+++ 2-strings-manipulation.py
@@ -14,54 +14,63 @@ Asígnale como valor una string con el nombre de una compañera".
 Crea una variable llamada "teammate-hobbie". 
 Asígnale como valor una string con unos de sus hobbies". 
 """
-# Escribe tu código aquí
+hobbie = "leer"
+name = "Irma"
+teammate = "Carmen"
+teammate_hobbie = "correr"
+
 
 
 """
 --- Ejercicio 2 Concatenación ---
 Imprime por consola el siguiente mensaje concatenando las variales anteriormente declaradas:
-"Soy [name] y en mis tiempos libres me gusta [hobbie]"
+"Soy " [name] y en mis tiempos libres me gusta [hobbie]"
 """
-# Escribe tu código aquí
+mensaje = "Soy " + name + " y en mis tiempos libres me gusta " + hobbie
+print(mensaje)
 
 """
 --- Ejercicio 3 f-strings ---
 Imprime por consola el siguiente mensaje usando f-strings para unir las frases de las variales anteriormente declaradas:
 "Ella es [teammate] y en sus tiempos libres le gusta [teammate-hobbie]"
 """
-# Escribe tu código aquí
+mensaje = f"Ella es {teammate} y en sus tiempos libres le gusta {teammate_hobbie}"
+print(mensaje)
 
 """
 --- Ejercicio 4 separación por comas ---
 Imprime por consola el siguiente mensaje usando separación por comas para unir las frases de las variales anteriormente declaradas:
 "Ella se llama [teammate] y yo me llamo [name]"
 """
-# Escribe tu código aquí
+print("Ella se llama", teammate, "y yo me llamo", name)
 
 """
 --- Ejercicio 5 separación con operador % ---
 Imprime por consola el siguiente mensaje usando separación con el operador % para unir las frases de las variales anteriormente declaradas:
 "Además de programar, nos gusta [hobbie] y [teammate-hobbie]"
 """
-# Escribe tu código aquí
+print("Además de programar, nos gusta %s y %s" % (hobbie, teammate_hobbie))
 
 """
 --- Ejercicio 6 input data ---
 Escribe dos variables que reciban por terminal un número cada una
 """
-# Escribe tu código aquí
+edad = input("Escribe tu edad")
+numero = input("Escribe tu numero")
 
 """
 --- Ejercicio 7 ---
 Imprime por consola el resultado de la suma de los dos número obtenidos anteriormente y 
 en un comentario de línea escribe lo que sucede. ¡Recuerda que puedes usar type() para indagar mas!
 """
-# Escribe tu código aquí
-# Escribe tu análisis aquí
+print(edad + numero)
+# No suma los números, sino que los concatena porque los considera texto, hay que convertir el texto en número con int()
 
 """
 --- Ejercicio 6 conversión de strings ---
 Transforma los valores recibidos en el ejercicio 6 a números
 Imprime por consola el resultado de la suma de los dos número obtenidos anteriormente
 """
-# Escribe tu código aquí
+edad = int(input())
+numero = int(input())
+print(edad + numero)
