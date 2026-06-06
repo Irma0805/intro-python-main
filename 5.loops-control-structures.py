eae4a0f91b7fd6c918cd2550564c89917ed703fe
Irma0805 <irmajortizs@hotmail.es>
2026-06-06 18:37:39 +0200

    Complete control flow exercises

--- 5.loops-control-structures.py
+++ 5.loops-control-structures.py
@@ -7,7 +7,12 @@ En este taller aprenderás usar los métodos más típicos para dirigir el flujo
 --- Ejercicio 1 condicionales  ---
 Escribe un programa que pida al usuario una letra y luego imprima un mensaje indicando si es una vocal o una consonante.
 """
-# Escribe tu código aquí
+letra = input("Ingresa una letra: ").lower()
+if letra in "aeiou":
+    print("es una vocal")
+else:
+    print("es una consonante")
+
 
 
 """
@@ -15,33 +20,57 @@ Escribe un programa que pida al usuario una letra y luego imprima un mensaje ind
 Escribe un programa que pida al usuario una nota (entre 0 y 100) y determine si 
 es una calificación de "A", "B", "C", "D" o "F".
 """
-# Escribe tu código aquí
+nota = int(input("Ingresa una nota: "))
+if nota >= 90:
+    print("A")
+elif nota >= 80:
+    print("B")
+elif nota >= 70:
+    print("C")
+elif nota >= 60:
+    print("D")
+else:
+    print("F")
 
 """
 --- Ejercicio 3  bucle while  ---
 Escribe un programa que pida al usuario un número entero positivo y 
 luego imprima la cuenta regresiva desde ese número hasta 1.
 """
-# Escribe tu código aquí
+numero =int(input("Introduce un número: "))
+while numero >= 1:
+    print(numero)
+    numero = numero - 1
+
 
 """
 --- Ejercicio 4  bucle for  ---
 Escribe un programa que imprima todos los caracteres de una cadena de texto ingresada por el usuario.
 """
-# Escribe tu código aquí
-
+texto = input("Introduce un texto: ")
+for letra in texto:
+    print(letra)
 """
 --- Ejercicio 5  bucle for con range ---
 Escribe un programa que imprima la tabla de multiplicar del 5 (del 1 al 10).
 """
-# Escribe tu código aquí
+for numero in range(1, 11):
+    print(numero * 5)
 
 """
 --- Ejercicio 6  bucle for con listas ---
 Escribe un programa que pida al usuario 5 palabras, las guarde en una lista y 
 luego en una nueva lista guarde todas las palabras en mayúsculas.
 """
-# Escribe tu código aquí
+palabras = []
+for numero in range(5):
+    palabra = input("Introduce un palabra: ")
+    palabras.append(palabra)
+
+palabras_mayusculas = []
+for palabra in palabras:
+    palabras_mayusculas.append(palabra.upper())
+print(palabras_mayusculas)
 
 
 """
@@ -52,4 +81,15 @@ si es un gato, que imprima en la consola "Tengo un gato",
 si es un pájaro, que imprima en la consola "Tengo un pájaro" y 
 si no es ninguno de los 3 que imprima "No tengo una mascota convencional"
 """
-# Escribe tu código aquí
+mascota = input("Introduce un mascota: ").lower()
+
+if mascota == "perro":
+    print("Tengo un perro")
+elif mascota == "gato":
+    print("Tengo un gato")
+elif mascota == "pajaro":
+    print("Tengo un pajaro")
+else:
+    print("No tengo mascota covencional")
+
+
